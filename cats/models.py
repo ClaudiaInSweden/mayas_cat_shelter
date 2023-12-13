b84d4ce77318e8989e79dca1bbda6dbd50b0c587
Claudia Bergstaller <claudia.bergstaller@gmx.com>
2023-12-13 01:12:17 +0000

    Create cats model and templates

--- cats/models.py
+++ cats/models.py
@@ -1,4 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
+
+
+STATUS = ((0, "Draft"), (1, "Published"))
+GENDER = ((0, "male"), (1, "female"))
+
+
+class Cats(models.Model):
+    id = models.IntegerField(primary_key=True, unique=True)
+    catname = models.CharField(max_length=20)
+    born = models.DateField()
+    gender = models.IntegerField(choices=GENDER, default=0)
+    description = models.TextField(blank=True)
+    image = CloudinaryField('image', default='placeholder')
+    status = models.IntegerField(choices=STATUS, default=0)
+
+
+
 
 # Create your models here.
--- cats/views.py
+++ cats/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
+from django.http import HttpResponseRedirect
+from .models import Cats
+
+
+class CatsDetail(View):
+    def get(self, request):
+        queryset = Cats.objects.filter(status=1)
+        cats = get_object_or_404(queryset, catname=catname)
+
+
+        return render(
+            request,
+            "cats.html",
+            {
+                "catname": catname,
+                "born": born,
+                "gender": gender,
+                "description": description,
+                "image": image
+            }
+        )
 
 # Create your views here.
